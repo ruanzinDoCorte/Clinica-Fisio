0e5511bfb31834608e0d45fc24441e9145f8956e
Ruan Rodrigues <rrodriguinho2528@gmail.com>
2025-06-09 16:43:57 -0300

    Delete fisio directory

--- fisio/src/main/java/com/clinica/fisio/repositories/FisioterapeutaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clinica.fisio.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clinica.fisio.entities.Fisioterapeuta;
-
-public interface FisioterapeutaRepository extends JpaRepository<Fisioterapeuta, Long> {
-
-}
--- fisio/src/main/java/com/clinica/fisio/repositories/MovimentoDeAgendaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clinica.fisio.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clinica.fisio.entities.MovimentoDeAgenda;
-
-public interface MovimentoDeAgendaRepository extends JpaRepository<MovimentoDeAgenda, Long> {
-
-}
